8a0060223bb75ce684a5ad98cf4d212446a1cf7f
HarishK501 <harishcse18501@gmail.com>
2021-04-06 18:51:24 +0530

    Added code for playground

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, redirect, url_for, session, render_template
 app = Flask(__name__)
 app.secret_key = "jcnuTadg273gd3ybd478"
 
+
+@app.route('/playground')
+def playground():
+    return render_template("playground.html")
+
 @app.route('/home')
 def home():
     return render_template("index.html")
